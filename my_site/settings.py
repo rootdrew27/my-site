b5fbbb648687815bc4b080b7a445b4b032e5fa92
Andrew Root <root.drew27@gmail.com>
2023-06-08 23:17:27 -0500

    Altered code for the purpose of making a resume rather than a blog site

--- my_site/settings.py
+++ my_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'projects_display',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_local_db',
+        'USER': 'rootdrew',
+        'PASSWORD': 'mypassword',
+        'HOST': '', 
+
+        
     }
 }
 
