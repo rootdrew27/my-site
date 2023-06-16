95390d3838c076bc28d34db2f64f2220fa7fac09
Andrew Root <root.drew27@gmail.com>
2023-06-15 23:05:19 -0500

    removed unecessary app. moved static/img to app dir

--- my_site/settings.py
+++ my_site/settings.py
@@ -38,7 +38,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ProjectsDisplay',
-    'portfolio',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -129,8 +128,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
 
 # Default primary key field type
