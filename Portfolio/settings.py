639b9d6b22e962a9e86b1803b9940efd984d25ed
Andrew Root <root.drew27@gmail.com>
2023-06-16 23:00:47 -0500

    added Blog Models

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ProjectsDisplay',
-    'blog',
+    'Blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
